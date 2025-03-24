f9f33e42b29099f4db593f47cd3427573bf18689
Raavi Kaur <raavi211299@gmail.com>
2025-03-24 16:32:15 -0700

    update app.jsx to take input from text field on button press add text display in list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+    const [inputText, setInput] = useState("");
+    const [items, setItems] = useState([]);
+
+    function handleChange(event){
+        const inputevent = event.target.value;
+        setInput(inputevent)
+    }
+    function addItem(){
+        setItems(inputText);
+        setInput("");
+    }
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-      <input type="text" />
-      <button>
+      <input type="text" onChange={handleChange} value={inputText}/>
+      <button onClick={addItem}>
         <span>Add</span>
       </button>
     </div>
       <div>
         <ul>
-          <li></li>
+          <li>{items}</li>
         </ul>
       </div>
     </div>
